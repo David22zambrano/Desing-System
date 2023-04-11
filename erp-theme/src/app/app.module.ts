5dbc6f6197613392311b7aed0b5dc3cb3c679732
David22zambrano <david.zambrano17@outlook.com>
2023-04-11 16:42:46 -0500

    Version 1.0 desing system ERP

---
+++ erp-theme/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { MaterialModule       } from 'src/theme/material.module';
+import { AppRoutingModule     } from './app-routing.module';
+import { AppComponent         } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
